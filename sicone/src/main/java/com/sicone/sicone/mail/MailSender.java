e54a15ac6af118db2377db5b768ba4053a281c4b
Douglas Tiepolo <dvtiepolo@gmail.com>
2018-09-12 21:35:27 -0300

    up

--- sicone/src/main/java/com/sicone/sicone/mail/MailSender.java
+++
@@ -1,9 +0,0 @@
-package com.sicone.sicone.mail;
-
-import javax.mail.MessagingException;
-
-public interface MailSender {
-	
-	void send(String to, String subject, String body) throws MessagingException;
-
-}
